8da288b21cc33b12cd43c98d6e81cc2c2e609693
flippingflippers <citrussunshine2004@gmail.com>
2018-07-31 18:51:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Welcome to my life!"
+end
+
+get '/name' do
+  "My name is Alexina Olivia Hill"
+end
+
+get '/hometown' do
+  "My hometown is Trenton, New jersey"
+end
+
+get '/favorite-song' do
+  "Mr. Brightside BY: TheKillersMusic"
+end
+
 end
